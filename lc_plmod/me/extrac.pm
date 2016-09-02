7b6f68612d00dc47b8bb73626d15cb55143c0d78
Sophia Shapira <Sophia.Shapira@gmail.com>
2016-09-02 13:40:51 -0400

    Small fix to extraction library

--- lc_plmod/me/extrac.pm
+++ lc_plmod/me/extrac.pm
@@ -2,17 +2,30 @@ package me::extrac;
 use strict;
 use chobak_json;
 use wraprg;
+use Scalar::Util qw(looks_like_number);
 
 sub hashfrom {
   my $lc_ref;
   my @lc_ret;
   my $lc_each;
+  my $lc_reps;
   
   $lc_ref = $_[0];
   @lc_ret = ();
   
   if ( ref($lc_ref) ne 'ARRAY' )  { return @lc_ret; }
   if ( $lc_ref->[0] eq 'cm' ) { return @lc_ret; }
+  $lc_reps = 1;
+  if ( $lc_ref->[0] eq 'rep' )
+  {
+    if ( looks_like_number($lc_ref->[1]) )
+    {
+      if ( ($lc_ref->[1]) > 1 )
+      {
+        $lc_reps = $lc_ref->[1];
+      }
+    }
+  }
   
   foreach $lc_each (@$lc_ref)
   {
@@ -40,6 +53,19 @@ sub hashfrom {
       if ( $lc3_a > 5 ) { @lc_ret = (@lc_ret,$lc_each); }
     }
     if ( ref($lc_each) eq 'ARRAY' ) { @lc_ret = (@lc_ret,&hashfrom($lc_each,$_[1])); }
+  }
+  
+  # Here we use the $lc_reps part:
+  {
+    my @lc2_a;
+    my $lc2_b;
+    @lc2_a = @lc_ret;
+    $lc2_b = $lc_reps;
+    while ( $lc2_b > 1.5 ) # Goes down to 1 instead of 0 - because one copy already is in there
+    {
+      @lc_ret = (@lc_ret,@lc2_a);
+      $lc2_b = int($lc2_b - 0.8);
+    }
   }
   
   return @lc_ret;
