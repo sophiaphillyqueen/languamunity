67abe9e5e16666fa6d4e53c90e703f0a3b1b0856
Sophia Shapira <Sophia.Shapira@gmail.com>
2016-10-09 18:33:49 -0400

    Can use OSX feature for command-lining Spanish speech However - this feature isn't yet in any way integrated into the quizes - though the plan is that it eventually will be as an optional.

--- inner-wrap.pl
+++ inner-wrap.pl
@@ -29,6 +29,25 @@ sub try_in_our_subdir {
   }
 }
 
+sub try_os_in_our_subdir {
+  my $lc_cmfl;
+  my $lc_osid;
+  my @lc_cmln;
+  
+  $lc_osid = `chobakwrap -sub osid`; chomp($lc_osid);
+  $lc_cmfl = &argola::srcd() . "/submd/os-" . $lc_osid . '-' . $comname . ".pl";
+  if ( -f $lc_cmfl )
+  {
+    @lc_cmln = ($lc_cmfl);
+    while ( &argola::yet() )
+    {
+      @lc_cmln = (@lc_cmln,&argola::getrg());
+    }
+    plelorec::flplex(@lc_cmln);
+    exit(0);
+  }
+}
+
 sub try_in_command_path {
   my $lc_scm;
   my $lc_loc;
@@ -49,6 +68,7 @@ sub try_in_command_path {
 
 # First we try to find it in our subdirectory:
 &try_in_our_subdir();
+&try_os_in_our_subdir();
 # Then we try to search the command path:
 &try_in_command_path();
 
---
+++ submd/os-osx-say--es.pl
@@ -0,0 +1,30 @@
+use strict;
+use argola;
+
+my $vario_gselc;
+my $vario_chosen;
+my $vario_csid;
+my $end_voice;
+
+my $vario_voxs = ['Juan','Angelica'];
+
+$vario_gselc = &argola::getrg();
+if ( !(&argola::yet()) ) { exit(0); }
+
+sub zoomify {
+  $vario_chosen = 0;
+  if ( $vario_gselc eq 'm' ) { $vario_csid = 0; $vario_chosen = 10; }
+  if ( $vario_gselc eq 'f' ) { $vario_csid = 1; $vario_chosen = 10; }
+  if ( $vario_chosen < 5 ) { $vario_csid = ((int(rand(80))) % 2); }
+  $end_voice = $vario_voxs->[$vario_csid];
+}
+if ( $vario_gselc eq 'r1' ) { &zoomify(); }
+
+
+while ( &argola::yet() )
+{
+  my $lc_cont;
+  if ( $vario_gselc ne 'r1' ) { &zoomify(); }
+  $lc_cont = &argola::getrg();
+  system("say","-v",$end_voice,(' ' . $lc_cont));
+}
