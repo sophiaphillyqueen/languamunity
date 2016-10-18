157996fff25cbe97a23f5355bf2ac13256705f34
Sophia Shapira <Sophia.Shapira@gmail.com>
2016-10-18 14:38:35 -0400

    Now options allow 's002-eligible' to see already-added eligible lessons

--- submd/cm-s002-eligible.pl
+++ submd/cm-s002-eligible.pl
@@ -20,6 +20,8 @@ my $index; # Data tree of the index file.
 my $lesson_belt; # The belt of all so-far-added lessons
 my $lesson_ctlg; # The catalogue of all lessons in the index file
 my $lesson_rec; # The record of this individual lesson
+my $elim_redun = 10; # By default - already-added lessons are not displayed
+my $elim_nuvos = 0; # By default - eligible not-already-added lessons are displayed
 
 # Here comes the litany by which the control file and then
 # the index file are opened.
@@ -35,6 +37,24 @@ if ( ! ( &chobak_jsonf::byref($arg_is_01,$cntrobj,$cntrpram) ) )
 $cntrd = $cntrobj->cont();
 $index = &chobak_json::readf($cntrd->{'indexfile'});
 
+sub opto__old__do {
+  $elim_redun = 0;
+}; &argola::setopt('-old',\&opto__old__do);
+
+sub opto__xold__do {
+  $elim_redun = 10;
+}; &argola::setopt('-xold',\&opto__xold__do);
+
+sub opto_new_do {
+  $elim_nuvos = 0;
+}; &argola::setopt('-new',\&opto_new_do);
+
+sub opto_xnew_do {
+  $elim_nuvos = 10;
+}; &argola::setopt('-xnew',\&opto_xnew_do);
+
+&argola::runopts();
+
 $lesson_belt = $cntrd->{'lcnon'};
 $lesson_ctlg = $index->{'lcnx'};
 
@@ -46,14 +66,19 @@ sub atempto {
   my $lc_lcnrec;
   my $lc_oldlcn;
   my $lc_ifc;
+  my $lc_redun;
+  
   $lc_lcnrec = $_[0];
   
   # We gots to make sure that this program does not output
   # the ID of any lesson already added:
+  $lc_redun = 0;
   foreach $lc_oldlcn (@$lesson_belt)
   {
-    if ( $lc_oldlcn eq $lc_lcnrec->{'id'} ) { return; }
+    if ( $lc_oldlcn eq $lc_lcnrec->{'id'} ) { $lc_redun = 10; }
   }
+  if ( $lc_redun > 5 ) { if ( $elim_redun > 5 ) { return; } }
+  if ( $lc_redun < 5 ) { if ( $elim_nuvos > 5 ) { return; } }
   
   # We also don't want to show anything who's prerequisites
   # are not met.
