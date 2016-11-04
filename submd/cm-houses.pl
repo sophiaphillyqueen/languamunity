86b64f2822cc8b051a805bfb8fdd9f2b112c2656
Sophia Shapira <Sophia.Shapira@gmail.com>
2016-11-04 05:59:03 -0400

    Added '-v' option to 'houses' subcommand.

--- submd/cm-houses.pl
+++ submd/cm-houses.pl
@@ -3,6 +3,19 @@ use argola;
 use me::pref_central;
 
 my $allhouses;
+my $show_method = 0;
+my $show_locat = 0;
+my $datasrc;
+
+sub opto__v_do {
+  $show_method = 10;
+  $show_locat = 10;
+} &argola::setopt('-v',\&opto__v_do);
+
+&argola::runopts();
+
+
+$datasrc = &me::pref_central::getdat();
 
 
 $allhouses = &me::pref_central::houses_alpb();
@@ -16,7 +29,23 @@ $allhouses = &me::pref_central::houses_alpb();
 }
 
 sub reporta {
-  system("echo",$_[0]);
+  my $lc_disp;
+  my $lc_srctyp;
+  
+  $lc_srctyp = $datasrc->{'houses'}->{$_[0]}->{'srctyp'};
+  
+  $lc_disp = $_[0];
+  if ( $show_method > 5 )
+  {
+    $lc_disp .= ' ' . $lc_srctyp;
+  }
+  
+  if ( $show_locat > 5 )
+  {
+    if ( $lc_srctyp eq 'git' ) { $lc_disp .= ' ' . $datasrc->{'houses'}->{$_[0]}->{'srcloc'}; }
+  }
+  
+  system("echo",$lc_disp);
 }
 
 
