04f975fe701860ee6270a3471f94498f3fba3a84
Sophia Shapira <Sophia.Shapira@gmail.com>
2016-11-15 07:29:53 -0500

    Finally -- the most rudimentary per-chapter documentation feature works!!

--- submd/cm-s002-lcnlect.pl
+++ submd/cm-s002-lcnlect.pl
@@ -3,6 +3,7 @@ use argola;
 use chobak_json;
 use chobak_jsonf;
 use me::format_cntrol;
+use chobak_flf;
 use chobinfodig;
 
 my $cntrpram;
@@ -35,8 +36,71 @@ argola::runopts();
 
 
 sub show_lesson_for_lit {
+  my $lc_lcnlist;
+  my $lc_eachlcn;
   
+  if ( ref($index) ne 'HASH' ) { return(1>2); }
+  $lc_lcnlist = $index->{'lcnx'};
+  if ( ref($lc_lcnlist) ne 'ARRAY' ) { return(1>2); }
+  foreach $lc_eachlcn (@$lc_lcnlist)
+  {
+    if ( &show_lesson_if_match_id($lc_eachlcn,$_[0]) ) { return(2>1); }
+  }
+  return(1>2);
 }
+
+sub show_lesson_if_match_id {
+  my $lc_thisid;
+  
+  if ( ref($_[0]) ne 'HASH' ) { return(1>2); }
+  $lc_thisid = $_[0]->{'id'};
+  if ( $lc_thisid ne $_[1] ) { return(1>2); }
+  
+  &show_lect_of_lcn($_[0]);
+  
+  return(2>1);
+}
+
+sub show_lect_of_lcn {
+  my $lc_lctlist;
+  my $lc_each;
+  
+  if ( ref($_[0]) ne 'HASH' ) { return(1>2); }
+  $lc_lctlist = $_[0]->{'lect'};
+  if ( ref($lc_lctlist) ne 'ARRAY' ) { return(1>2); }
+  foreach $lc_each (@$lc_lctlist)
+  {
+    &show_lect_item($lc_each);
+  }
+  return(2>1);
+}
+
+sub show_lect_item {
+  my $lc_typ;
+  
+  if ( ref($_[0]) ne 'HASH' ) { return(1>2); }
+  if ( !(defined($_[0]->{'typ'})) )
+  {
+    die "\nFATAL ERROR: Lect item with undefined type:\n\n";
+  }
+  $lc_typ = $_[0]->{'typ'};
+  
+  if ( $lc_typ eq 'relhtm' )
+  {
+    my $lc2_a;
+    my $lc2_b;
+    
+    $lc2_a = &chobak_flf::realpath(&chobak_flf::k_gdr($cntrd->{'indexfile'}));
+    $lc2_b = $lc2_a . '/' . $_[0]->{'loc'};
+    system("chobakwrap",'-sub','browseropen',$lc2_b);
+    
+    return(2>1);
+  }
+  
+  die("\nFATAL ERROR: Unknown lecture-item type: " . $lc_typ . ":\n\n");
+}
+
+
 
 
 
